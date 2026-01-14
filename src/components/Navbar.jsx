74bd2ff133e7e97452b30befa09e91ecefe51ded
Jordan Buckley <Jordanbuckleycork@gmail.com>
2026-01-14 21:02:00 +0000

    Final touches

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -139,6 +139,7 @@ function Navbar() {
           {[
             { href: "#skills", label: "Skills" },
             { href: "#education", label: "Education" },
+            { href: "#experiences", label: "Experiences" },
             { href: "#achievements", label: "Achievements" },
             { href: "#volunteering", label: "Volunteering" },
             { href: "#projects", label: "Projects" },
